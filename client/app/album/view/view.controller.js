f8ed5920d49e833a6b930feaa84ca10cf57a8c2c
Kevin van der Vlist <kvdvlist@sogyo.nl>
2014-07-16 21:11:23 +0200

    This is a way better approach to this asynchronous problem

--- client/app/album/view/view.controller.js
+++ client/app/album/view/view.controller.js
@@ -5,34 +5,54 @@ function humanReadableDate(date) {
   return d.getDay() + '-' + d.getMonth() + '-' + d.getFullYear();
 }
 
+function getAlbumDetails(rx, $q, $http, albumDetailsPromise, deferredMediaDetails) {
+  var deferredAlbum = $q.defer();
+
+  rx.Observable
+    .fromPromise(albumDetailsPromise)
+    .map(function(response){
+      return response.data;
+    })
+    .subscribe(function(album) {
+      $http.get('/api/media/' + album._id).
+        then(function(result) {
+          deferredMediaDetails.resolve(result);
+        });
+
+      album.startDateStr = humanReadableDate(album.startDate);
+      album.endDateStr = humanReadableDate(album.startDate);
+      deferredAlbum.resolve(album);
+    });
+
+  return deferredAlbum.promise;
+}
+
+function getMediaDetails(rx, mediaDetailsPromise) {
+  return rx.Observable
+    .fromPromise(mediaDetailsPromise)
+    .map(function(response) {
+      return response.data;
+    })
+    .flatMap(rx.Observable.fromArray);
+}
+
 angular.module('seriousBoomerangApp')
-  .controller('AlbumViewCtrl', function ($scope, $stateParams, $http) {
+  .controller('AlbumViewCtrl', function ($scope, $stateParams, $http, $q) {
     var rx = Rx; // jshint ignore:line
     $scope.album = null;
     $scope.images = [];
 
     var albumDetails = $http.get('/api/album/' + $stateParams.year + '/' + $stateParams.name);
+    var deferredMediaDetails = $q.defer();
 
-    rx.Observable
-      .fromPromise(albumDetails)
-      .map(function(response){
-        return response.data;
-      })
-      .subscribe(function(album) {
-        var mediaDetails = $http.get('/api/media/' + album._id);
+    var albumPromise = getAlbumDetails(rx, $q, $http, albumDetails, deferredMediaDetails);
+    var mediaObservable = getMediaDetails(rx, deferredMediaDetails.promise);
 
-        album.startDateStr = humanReadableDate(album.startDate);
-        album.endDateStr = humanReadableDate(album.startDate);
-        $scope.album = album;
+    albumPromise.then(function(album) {
+      $scope.album = album;
+    });
 
-        rx.Observable
-          .fromPromise(mediaDetails)
-          .map(function(response) {
-            return response.data;
-          })
-          .flatMap(rx.Observable.fromArray)
-          .subscribe(function(media) {
-            $scope.images.push(media);
-          })
-      });
+    mediaObservable.subscribe(function(media) {
+      $scope.images.push(media);
+    });
   });
